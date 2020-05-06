dc6c448c527ed28e20756efa414d108269bd36d2
Patrick Branch <branch.patrick@gmail.com>
2020-05-06 11:33:55 -0500

    Add template conf file and instructions in it.  This will be made more user-friendly later.

---
+++ conf.template.py
@@ -0,0 +1,12 @@
+#####
+# Copy this file to conf.py and provide the correct details for your system
+#####
+
+# path to firefox executable
+BROWSER_EXE = '/Applications/Firefox.app/Contents/MacOS/firefox-bin'
+# path to geckodriver executable
+GECKODRIVER = '/usr/local/bin/geckodriver'
+
+# Facebook credentials
+LOGIN_EMAIL = <enter facebook username here>
+LOGIN_PASSWORD = <enter facebook passwordhere>
